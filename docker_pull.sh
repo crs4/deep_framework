1ee149bdb30bcfa1e53cf358f497857624c7be49
asassu <alex.sassu@tiscali.it>
2020-06-29 18:05:48 +0200

    auto docker pull

--- docker_pull.sh
+++ docker_pull.sh
@@ -1,30 +1,27 @@
-
-
 #!/bin/bash
-
 registry="$1"
-
-
-
-docker pull $registry/face_detector:deep
+docker pull $registry/monitor:deep
 docker pull $registry/collector:deep
+docker pull $registry/server:deep
+docker pull $registry/stream_manager:deep
+docker pull $registry/person_detection:deep
+docker pull $registry/drone_object_detector:deep_cpu
+docker pull $registry/drone_object_detector:deep_gpu
+docker pull $registry/face_detection:deep_cpu
+docker pull $registry/gender_detection:deep_cpu
+docker pull $registry/gender_detection:deep_gpu
+docker pull $registry/glasses_detection:deep_cpu
+docker pull $registry/glasses_detection:deep_gpu
+docker pull $registry/emotion_detection:deep_cpu
+docker pull $registry/emotion_detection:deep_gpu
+docker pull $registry/face_recognition:deep_cpu
+docker pull $registry/face_recognition:deep_gpu
+docker pull $registry/pitch_detection:deep_cpu
+docker pull $registry/pitch_detection:deep_gpu
+docker pull $registry/age_detection:deep_cpu
+docker pull $registry/age_detection:deep_gpu
+docker pull $registry/yaw_detection:deep_cpu
+docker pull $registry/yaw_detection:deep_gpu
 docker pull $registry/broker:deep
 docker pull $registry/sub_collector:deep
-docker pull $registry/monitor:deep
-docker pull $registry/yaw:deep_cpu
-docker pull $registry/face_recognition:deep_cpu
-docker pull $registry/age:deep_cpu
-docker pull $registry/emotion:deep_cpu
-docker pull $registry/gender:deep_cpu
-docker pull $registry/glasses:deep_cpu
-docker pull $registry/pitch:deep_cpu
-docker pull $registry/yaw:deep_gpu
-docker pull $registry/face_recognition:deep_gpu
-docker pull $registry/age:deep_gpu
-docker pull $registry/emotion:deep_gpu
-docker pull $registry/gender:deep_gpu
-docker pull $registry/glasses:deep_gpu
-docker pull $registry/pitch:deep_gpu
-docker pull $registry/server:deep
 docker pull $registry/stream_capture:deep
-docker pull $registry/stream_manager:deep
--- initialize/starter.py
+++ initialize/starter.py
@@ -34,7 +34,10 @@ class Starter:
 	def build_and_push(self):
 		print('The building process will take several time...')
 		self.image_manager.build_images()
+		self.image_manager.create_pull_file()
+
 		self.image_manager.push_images()
+
 
 
 	def manage_registry(self):
@@ -187,6 +190,7 @@ class ImageManager:
 		self.machine = machine
 		self.registry = registry.insecure_addr
 		self.images_list = []
+		self.__pull_images=[]
 	
 	def __find_dockerfiles(self):
 		dockerfiles_path = []
@@ -226,7 +230,8 @@ class ImageManager:
 				image_name = self.registry + '/' + temp_image_name + tag
 			t_flag = ' -t ' + image_name
 			self.images_list.append(image_name)
-
+			if 'setup' not in tag:
+				self.__pull_images.append(temp_image_name + tag)
 			build_command = base_com + f_flag + t_flag + context
 			if 'setup' in t_flag:
 				build_commands = [build_command] + build_commands
@@ -238,48 +243,6 @@ class ImageManager:
 		paths = self.__find_dockerfiles()
 		build_commands = self.__create_build_commands(paths)
 		
-
-		"""
-		base_com = 'docker build '
-		build_utils = base_com + '-t utils:deep utils/'
-		build_agent = base_com + '-t agent:deep descriptor/agent'
-		build_recog_setup = base_com + '-f descriptor/feature_extractors/face_recognition/Dockerfile.recognition_setup' + ' -t face_recognition_setup:deep descriptor/feature_extractors/face_recognition/'
-
-
-		build_face_detector  = base_com + '-t '+self.registry+'/face_detection:deep detector/face_detection'
-		build_person_detector  = base_com + '-t '+self.registry+'/person_detection:deep detector/person_detection'
-		build_object_detector_cpu  = base_com +'-f detector/drone_object_detector/cpu.Dockerfile '+  '-t '+self.registry+'/drone_object_detector:deep_cpu detector/drone_object_detector'
-		build_object_detector_gpu  = base_com  +'-f detector/drone_object_detector/gpu.Dockerfile '+ '-t '+self.registry+'/drone_object_detector:deep_gpu detector/drone_object_detector'
-		
-
-
-
-		build_collector = base_com + '-t '+self.registry+'/collector:deep collector/'
-		build_broker = base_com + ' -t '+self.registry+'/broker:deep descriptor/broker'
-		build_sub_col = base_com + ' -t '+self.registry+'/sub_collector:deep descriptor/sub_collector'
-		build_monitor = base_com + ' -t '+self.registry+'/monitor:deep monitor/'
-
-		build_yaw_cpu = base_com + '-f descriptor/feature_extractors/yaw_detection/Dockerfile.yaw_detection_cpu' + ' -t '+self.registry+'/yaw:deep_cpu descriptor/feature_extractors/yaw_detection/'
-		build_recog_cpu = base_com + '-f descriptor/feature_extractors/face_recognition/Dockerfile.recognition_cpu' + ' -t '+self.registry+'/face_recognition:deep_cpu descriptor/feature_extractors/face_recognition/'
-		build_age_cpu = base_com + '-f descriptor/feature_extractors/age_detection/Dockerfile.age_cpu' + ' -t '+self.registry+'/age:deep_cpu descriptor/feature_extractors/age_detection'
-		build_emotion_cpu = base_com + '-f descriptor/feature_extractors/emotion_detection/Dockerfile.emotion_cpu' + ' -t '+self.registry+'/emotion:deep_cpu descriptor/feature_extractors/emotion_detection/'
-		build_gender_cpu = base_com + '-f descriptor/feature_extractors/gender_detection/Dockerfile.gender_cpu' + ' -t '+self.registry+'/gender:deep_cpu descriptor/feature_extractors/gender_detection'
-		build_glasses_cpu = base_com + '-f descriptor/feature_extractors/glasses_detection/Dockerfile.glasses_recognition_cpu' + ' -t '+self.registry+'/glasses:deep_cpu descriptor/feature_extractors/glasses_detection/'
-		build_pitch_cpu = base_com + '-f descriptor/feature_extractors/pitch_detection/Dockerfile.pitch_detection_cpu' + ' -t '+self.registry+'/pitch:deep_cpu descriptor/feature_extractors/pitch_detection/'
-
-		build_yaw_gpu = base_com + '-f descriptor/feature_extractors/yaw_detection/Dockerfile.yaw_detection_gpu'+ ' -t '+self.registry+'/yaw:deep_gpu descriptor/feature_extractors/yaw_detection/'
-		build_recog_gpu = base_com + '-f descriptor/feature_extractors/face_recognition/Dockerfile.recognition_gpu'+ ' -t '+self.registry+'/face_recognition:deep_gpu descriptor/feature_extractors/face_recognition/'
-		build_age_gpu = base_com + '-f descriptor/feature_extractors/age_detection/Dockerfile.age_gpu' + ' -t '+self.registry+'/age:deep_gpu descriptor/feature_extractors/age_detection'
-		build_emotion_gpu = base_com + '-f descriptor/feature_extractors/emotion_detection/Dockerfile.emotion_gpu' + ' -t '+self.registry+'/emotion:deep_gpu descriptor/feature_extractors/emotion_detection/'
-		build_gender_gpu = base_com + '-f descriptor/feature_extractors/gender_detection/Dockerfile.gender_gpu' + ' -t '+self.registry+'/gender:deep_gpu descriptor/feature_extractors/gender_detection'
-		build_glasses_gpu = base_com + '-f descriptor/feature_extractors/glasses_detection/Dockerfile.glasses_recognition_gpu' + ' -t '+self.registry+'/glasses:deep_gpu descriptor/feature_extractors/glasses_detection/'
-		build_pitch_gpu = base_com + '-f descriptor/feature_extractors/pitch_detection/Dockerfile.pitch_detection_gpu' + ' -t '+self.registry+'/pitch:deep_gpu descriptor/feature_extractors/pitch_detection/'
-		
-		build_server = base_com + ' -t '+self.registry+'/server:deep server/'
-		build_stream_capture = base_com + ' -t '+self.registry+'/stream_capture:deep stream_capture/'
-		build_stream_manager = base_com + ' -t '+self.registry+'/stream_manager:deep stream_manager/'
-		build_commands = [build_utils,build_agent,build_recog_setup,build_face_detector,build_collector,build_broker,build_sub_col,build_monitor,build_yaw_gpu,build_recog_gpu,build_age_gpu,build_emotion_gpu,build_gender_gpu,build_glasses_gpu,build_pitch_gpu,build_yaw_cpu,build_recog_cpu,build_age_cpu,build_emotion_cpu,build_gender_cpu,build_glasses_cpu,build_pitch_cpu,build_server,build_stream_capture,build_stream_manager,build_person_detector,build_object_detector_cpu,build_object_detector_gpu]
-		"""
 		for i,build in enumerate(build_commands):
 			
 			if 'cpu' in build:
@@ -303,36 +266,7 @@ class ImageManager:
 			self.machine.exec_shell_command(build,ignore_err = False)
 
 	def push_images(self):
-		"""
-		base_com = 'docker push '
-		push_face_detector = base_com  +self.registry+'/face_detection:deep'
-		push_person_detector = base_com  +self.registry+'/person_detection:deep'
-		push_obj_detector_cpu = base_com  +self.registry+'/drone_object_detector:deep_cpu'
-		push_obj_detector_gpu = base_com  +self.registry+'/drone_object_detector:deep_gpu'
-		push_collector = base_com  +self.registry+'/collector:deep'
-		push_broker = base_com  +self.registry+'/broker:deep'
-		push_sub_collector = base_com  +self.registry+'/sub_collector:deep'
-		push_monitor = base_com  +self.registry+'/monitor:deep'
-		push_yaw_cpu = base_com  +self.registry+'/yaw:deep_cpu'
-		push_face_recognition_cpu = base_com  +self.registry+'/face_recognition:deep_cpu'
-		push_age_cpu = base_com  +self.registry+'/age:deep_cpu'
-		push_emotion_cpu = base_com  +self.registry+'/emotion:deep_cpu'
-		push_gender_cpu = base_com  +self.registry+'/gender:deep_cpu'
-		push_glasses_cpu = base_com  +self.registry+'/glasses:deep_cpu'
-		push_pitch_cpu = base_com  +self.registry+'/pitch:deep_cpu'
-		push_yaw_gpu = base_com  +self.registry+'/yaw:deep_gpu'
-		push_face_recognition_gpu = base_com  +self.registry+'/face_recognition:deep_gpu'
-		push_age_gpu = base_com  +self.registry+'/age:deep_gpu'
-		push_emotion_gpu = base_com  +self.registry+'/emotion:deep_gpu'
-		push_gender_gpu = base_com  +self.registry+'/gender:deep_gpu'
-		push_glasses_gpu = base_com  +self.registry+'/glasses:deep_gpu'
-		push_pitch_gpu = base_com  +self.registry+'/pitch:deep_gpu'
-		push_server = base_com  +self.registry+'/server:deep'
-		push_stream_capture = base_com  +self.registry+'/stream_capture:deep'
-		push_stream_manager = base_com  +self.registry+'/stream_manager:deep'
 		
-		push_commands = [push_face_detector,push_person_detector,push_obj_detector_cpu,push_obj_detector_gpu,push_collector,push_broker,push_sub_collector,push_monitor,push_yaw_cpu,push_face_recognition_cpu,push_age_cpu,push_emotion_cpu,push_gender_cpu,push_glasses_cpu,push_pitch_cpu,push_yaw_gpu,push_face_recognition_gpu,push_age_gpu,push_emotion_gpu,push_gender_gpu,push_glasses_gpu ,push_pitch_gpu,push_server,push_stream_capture ,push_stream_manager]
-		"""
 		push_commands = self.__create_push_commands()
 		for i,push in enumerate(push_commands):
 
@@ -354,3 +288,20 @@ class ImageManager:
 
 			print('Pushing %s %s: %s of %s' % (image_name,mode, str(i+1), str(len(push_commands))))
 			self.machine.exec_shell_command(push)
+
+	def create_pull_file(self):
+		filename = 'docker_pull.sh'
+		base_com = 'docker pull $registry/'
+		with open(filename, 'w') as pull_file:
+			pull_file.write('#!/bin/bash\n')
+			pull_file.write('registry="$1"\n')
+			for img in self.__pull_images:
+				pull_file.write(base_com + img +'\n')
+
+
+
+
+
+
+
+
