b1891dfde4d4bc646a0640fb11feebf4529a6da5
ale_cadmio <asassu@crs4.it>
2020-10-15 16:15:32 +0200

    update gputils

--- initialize/gputils.py
+++ initialize/gputils.py
@@ -49,7 +49,11 @@ class GPUallocator(Machine):
 		index = data.find('Driver Version')
 		non_decimal = re.compile(r'[^\d.]+')
 		temp_version = data[index+16:index+30]
-		driver_version = float(non_decimal.sub('', temp_version))
+		str_driver_version = non_decimal.sub('', temp_version)
+		splt = str_driver_version.split('.')
+		driver_version = float(splt[0]+'.'+splt[1])
+		print(driver_version)
+		a = input('stop')
 		if driver_version < self.driver_version_thr:
 			print('Node %s: NVIDIA DRIVER version %s is too old. This node will be used in CPU mode.' % (node_name,driver_version))
 			return False
